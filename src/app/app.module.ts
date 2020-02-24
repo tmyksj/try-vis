8febcc3a6ae91e3376c710db9d15fb5a073e7386
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-24 14:50:09 +0900

    デザインの調整とリンク設計の変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -26,13 +26,13 @@ import { PageItemListComponent } from "./components/page-item-list/page-item-lis
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
-    MatCardModule,
+    MatButtonModule,
     MatTabsModule,
     MatToolbarModule,
     RouterModule.forRoot([
       { path: "", component: PageHomeComponent },
-      { path: "item/:id", component: PageItemComponent },
-      { path: "item-list", component: PageItemListComponent },
+      { path: "items", component: PageItemListComponent },
+      { path: "items/:id", component: PageItemComponent },
     ]),
   ],
   providers: [
