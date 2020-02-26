a369dd410b362e5738978f8c103bd9e2fdd4652f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-26 15:50:00 +0900

    ステップ数を表示

--- src/app/components/page-home/page-home.component.ts
+++ src/app/components/page-home/page-home.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { map, mergeMap, reduce } from "rxjs/operators";
+import { from, Observable } from "rxjs";
 
 import { ItemDomain } from "../../domains/item/item.domain";
 import { ItemDto } from "../../dtos/item/item.dto";
+import { ItemLogDto } from "../../dtos/item-log/item-log.dto";
+import { ItemLogTypeDto } from "../../dtos/item-log-type/item-log-type.dto";
 
 @Component({
   selector: "app-page-home",
@@ -12,6 +16,8 @@ export class PageHomeComponent implements OnInit {
 
   public recommendedItemList: ItemDto[];
 
+  public steps: number;
+
   private itemDomain: ItemDomain;
 
   public constructor(itemDomain: ItemDomain) {
@@ -21,6 +27,33 @@ export class PageHomeComponent implements OnInit {
   public ngOnInit(): void {
     this.itemDomain.recommendedItemList().subscribe((itemList: ItemDto[]): void => {
       this.recommendedItemList = itemList;
+    });
+
+    this.itemDomain.itemList().pipe(
+      mergeMap((itemList: ItemDto[]): Observable<ItemDto> => {
+        return from(itemList);
+      }),
+      mergeMap((item: ItemDto): Observable<ItemLogDto[]> => {
+        return this.itemDomain.itemLogList(item);
+      }),
+      mergeMap((itemLog: ItemLogDto[]): Observable<ItemLogDto> => {
+        return from(itemLog);
+      }),
+      map((itemLog: ItemLogDto): number => {
+        switch (itemLog.type) {
+          case ItemLogTypeDto.DoneToday:
+            return 1;
+          case ItemLogTypeDto.Done:
+            return 2;
+          default:
+            return 0;
+        }
+      }),
+      reduce((acc: number, value: number): number => {
+        return acc + value;
+      }),
+    ).subscribe((steps: number): void => {
+      this.steps = steps;
     });
   }
 
