7d96c7735c01cb029324c7f8534fd4ec9df6727d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-29 12:08:34 +0900

    yarn run ng generate component components/part-item-list

---
+++ src/app/components/part-item-list/part-item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-part-item-list',
+  templateUrl: './part-item-list.component.html',
+  styleUrls: ['./part-item-list.component.scss']
+})
+export class PartItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
