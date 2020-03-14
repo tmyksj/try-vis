3d849437bb05e1cd5bb892ecb16a2a7380d843ae
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-14 14:04:44 +0900

    グラフの描画

--- src/app/components/page-home/page-home.component.ts
+++ src/app/components/page-home/page-home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Chart } from "chart.js";
 
 import { ItemDomain } from "../../domains/item/item.domain";
 import { ItemDto } from "../../dtos/item/item.dto";
@@ -8,16 +9,73 @@ import { ItemDto } from "../../dtos/item/item.dto";
   styleUrls: ["./page-home.component.scss"],
   templateUrl: "./page-home.component.html",
 })
-export class PageHomeComponent implements OnInit {
+export class PageHomeComponent implements AfterViewInit, OnInit {
 
   public recommendedItemList: ItemDto[];
 
   public steps: number;
 
+  @ViewChild("canvas")
+  private canvas: ElementRef;
+
+  private chart: Chart;
+
   private itemDomain: ItemDomain;
 
   public constructor(itemDomain: ItemDomain) {
     this.itemDomain = itemDomain;
+  }
+
+  public ngAfterViewInit(): void {
+    const element: any = this.canvas.nativeElement;
+    if (!(element instanceof HTMLCanvasElement)) {
+      return;
+    }
+
+    const context: CanvasRenderingContext2D | null = element.getContext("2d");
+    if (context === null) {
+      return;
+    }
+
+    this.itemDomain.accumulateDailyStepsIntegrally().subscribe((dailySteps: [Date, number][]): void => {
+      this.chart = new Chart(context, {
+        data: {
+          datasets: [
+            {
+              data: dailySteps.map((dailyStep: [Date, number]): { x: Date, y: number } => {
+                return {
+                  x: dailyStep[0],
+                  y: dailyStep[1],
+                };
+              }),
+            },
+          ],
+        },
+        options: {
+          legend: {
+            display: false,
+          },
+          scales: {
+            xAxes: [
+              {
+                type: "time",
+                time: {
+                  unit: "day",
+                },
+              },
+            ],
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+        type: "line",
+      });
+    });
   }
 
   public ngOnInit(): void {
